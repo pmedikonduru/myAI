2e49c7ecdc760946f22f12bdfa6c625197e1c54e
pmedikonduru <159820343+pmedikonduru@users.noreply.github.com>
2025-03-06 20:49:15 -0600

    Update ui.ts
    fixed missing quotes

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -3,7 +3,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 export const CHAT_HEADER: string = `Pierre, your trusted cycling companion!`;
 export const MESSAGE_PLACEHOLDER: string = `Got a cycling query? Ask away!`;
 export const FOOTER_MESSAGE: string = `Crafted by Pranav @UNC`;
-export const CLEAR_BUTTON_TEXT: string = Clear Conversation;
+export const CLEAR_BUTTON_TEXT: string = 'Clear Conversation';
 export const PAGE_TITLE: string = `Road Bike Recommender`;
 export const PAGE_DESCRIPTION: string = `Looking for the perfect ride? I'm ${AI_NAME}, here to guide you through the world of road cycling with expert advice.`;
 
