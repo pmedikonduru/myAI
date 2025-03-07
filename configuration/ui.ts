2d93392b9ed67c538dd8bfe0802b0ab8d3a62320
pmedikonduru <159820343+pmedikonduru@users.noreply.github.com>
2025-03-06 19:24:54 -0600

    Update ui.ts
    personalized UI

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,8 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Pierre, your local bike expert!`;
-export const MESSAGE_PLACEHOLDER: string = `Ask me anything`;
-export const FOOTER_MESSAGE: string = `Customized by Pranav Medikonduru @UNC`;
-export const CLEAR_BUTTON_TEXT: string = `Clear Conversation`;
+export const CHAT_HEADER: string = `Pierre, your trusted cycling companion!`;
+export const MESSAGE_PLACEHOLDER: string = `Got a cycling query? Ask away!`;
+export const FOOTER_MESSAGE: string = `Crafted with passion by Pranav @UNC`;
 export const PAGE_TITLE: string = `Road Bike Recommender`;
-export const PAGE_DESCRIPTION: string = `Looking for a new bike? I'm ${AI_NAME}, here to help.`;
-
-export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
+export const PAGE_DESCRIPTION: string = `Looking for the perfect ride? I'm ${AI_NAME}, here to guide you through the world of road cycling with expert advice.`;
+export const EMPTY_CITATION_MESSAGE: string = "Source not specified";
