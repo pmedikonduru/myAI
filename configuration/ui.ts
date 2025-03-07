d6fdc9fc2ec21fae668bdd3a0282378356b4b91e
pmedikonduru <159820343+pmedikonduru@users.noreply.github.com>
2025-03-06 20:46:40 -0600

    Update ui.ts
    fixing error with no button text

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,8 +2,9 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Pierre, your trusted cycling companion!`;
 export const MESSAGE_PLACEHOLDER: string = `Got a cycling query? Ask away!`;
-export const FOOTER_MESSAGE: string = `Crafted with passion by Pranav @UNC`;
+export const FOOTER_MESSAGE: string = `Crafted by Pranav @UNC`;
 export const CLEAR_BUTTON_TEXT: string = Clear Conversation;
 export const PAGE_TITLE: string = `Road Bike Recommender`;
 export const PAGE_DESCRIPTION: string = `Looking for the perfect ride? I'm ${AI_NAME}, here to guide you through the world of road cycling with expert advice.`;
-export const EMPTY_CITATION_MESSAGE: string = "Source not specified";
+
+export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
